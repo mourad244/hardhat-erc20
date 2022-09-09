837474f7821a88751757fbf0861b728b42a617dd
mourad244 <mouradvilla2@gmail.com>
2022-09-09 21:07:59 +0100

    add

---
+++ hardhat.config.ts
@@ -0,0 +1,38 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.10",
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY != undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY != undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const ERC20 = await ethers.getContractFactory("ERC20");
+  const erc20 = await ERC20.deploy("Hello Token", "HEL");
+
+  await erc20.deployed();
+
+  console.log(`Greeter deployed to ${erc20.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
